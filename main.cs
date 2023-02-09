a4778410a2a61673fe4360634707f177e672b8b7
kanetrad <121792794+kanetrad@users.noreply.github.com>
2023-02-09 22:10:14 +0500

    Add files via upload

--- main.cs
+++ main.cs
@@ -24,6 +24,15 @@ namespace Workspace
 			int randomSword = random.Next(2,5);
 			int randomJustice = random.Next(1,2);
 
+			while (bot_amount != 5)
+			{
+				int battleID = random.Next(1, 5);
+				int bot = battleID;
+				string resultofid = "bot" + battleID.ToString();
+				bot_amount++;
+				Console.WriteLine(resultofid);
+			}
+
 			if (dPower>60)
 			{
 				int attack = random.Next(50,100);
@@ -60,51 +69,41 @@ namespace Workspace
 				}
 			}
 
-			if (dPower<60)
+			if (dPower<=60)
 			{
-				int attack = random.Next(20,90);
+				int attack_power = random.Next(20,90);
 				int bot_random = random.Next(1,5);
 				if (bot_random == 1)
 				{
 					int bot1 = 1;
-					Console.WriteLine("Lucky is "+bot1.ToString());
+					Console.WriteLine("Not so lucky is "+bot1.ToString());
 					int bot_lucky = bot1;
 				}
 				if (bot_random == 2)
 				{
 					int bot2 = 2;
-					Console.WriteLine("Lucky is "+bot2.ToString());
+					Console.WriteLine("Not so lucky is "+bot2.ToString());
 					int bot_lucky = bot2;
 				}
 				if (bot_random == 3)
 				{
 					int bot3 = 3;
-					Console.WriteLine("Lucky is "+bot3.ToString());
+					Console.WriteLine("Not so lucky is "+bot3.ToString());
 					int bot_lucky = bot3;
 				}
 				if (bot_random == 4)
 				{
 					int bot4 = 4;
-					Console.WriteLine("Lucky is "+bot4.ToString());
+					Console.WriteLine("Not so lucky is "+bot4.ToString());
 					int bot_lucky = bot4;
 				}
 				if (bot_random == 5)
 				{
 					int bot5 = 5;
-					Console.WriteLine("Lucky is "+bot5.ToString());
+					Console.WriteLine("Not so lucky is "+bot5.ToString());
 					int bot_lucky = bot5;
 				}
 			}
-
-			while (bot_amount != 5)
-			{
-				int battleID = random.Next(1, 5);
-				string resultofid = "bot" + battleID.ToString();
-				bot_amount++;
-				Console.WriteLine(resultofid);
-			}
-
-			if ()
 
 			Console.ReadLine();
 		}
