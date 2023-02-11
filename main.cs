4a63513f9f5151c42303cbbe8c9a4439b87f8279
kanetrad <121792794+kanetrad@users.noreply.github.com>
2023-02-11 22:32:08 +0500

    Add files via upload

--- main.cs
+++ main.cs
@@ -16,131 +16,75 @@ namespace Workspace
 		{
 			Random random = new Random();
 
-			//string bot_amount = Console.ReadLine();
 			Console.WriteLine("Enter bot amount: ");
 			int bot_amount = Int32.Parse(Console.ReadLine());
 			int FFF = 1;
 			int[] bots = {1,2,3,4,5,6,7,8,9,10};
 			int botHP = 100;
-			//int TWO = 0;
-			//int bot_amount = Convert.ToInt32(bot_amount_str);
+			int botHP2 = 100;
+			const int botHP_const = 100;
 			int power = random.Next(1, 10);
 			int dPower = random.Next(1, bot_amount+100);
-			//int randomJustice = random.Next(1,2);
-			int battleID;
-			int bot;
 			int randomBOT = random.Next(1, bots.Length);
+			int battleID = random.Next(1, bot_amount);
+			int bot = battleID;
+			int battleID2 = random.Next(1, bot_amount);
+
+			void changeTurn()
+			{
+				while (botHP > 0)
+				{
+					botHP = botHP - power;
+					Console.WriteLine("bot"+battleID+" was damaged by "+"bot"+battleID2+" for -"+random.Next(1,10));
+					changeTurn2();
+				}
+			}
+
+			void changeTurn2()
+			{
+				while (botHP2 > 0)
+				{
+					botHP2 = botHP2 - random.Next(1, 10);
+					Console.WriteLine("   bot"+battleID2+" was damaged by "+"bot"+battleID+" for -"+random.Next(1,10));
+					changeTurn();
+				}
+			}
 
 			while (FFF != bot_amount+1)
 			{
-				battleID = random.Next(1, bot_amount);
-				bot = battleID;
-				int battleID2 = random.Next(1, bot_amount);
-				string result = "bot" + battleID;
+				//string result = "bot" + random.Next(1, bot_amount);
 				FFF++;
-				Console.WriteLine(result);
-
+				//Console.WriteLine(result);
+			
 				if (FFF == bot_amount+1)
 				{
 					Console.WriteLine("bot"+battleID+" VS "+"bot"+battleID2);
-					Console.WriteLine("!!!   Bots have been split into 2 people   !!!");
+					//Console.WriteLine("!!!   Bots have been split into 2 people   !!!");
 
 					while (botHP > 0)
 					{
 						botHP = botHP - power;
-						Console.WriteLine("bot"+battleID+" was damaged by "+"bot"+battleID2+" for -"+random.Next(1, 10));
+						//Console.WriteLine("bot"+battleID+" was damaged by "+"bot"+battleID2+" for -"+power);
+						while (botHP < botHP_const)
+						{
+							changeTurn2();
+							break;
+						}
+						while (botHP2 < botHP_const)
+						{
+							changeTurn();
+							break;
+						}
 						if (botHP <= 0)
 						{
+							botHP = 0;
 							Console.WriteLine("!!!   bot"+randomBOT+" is dead   !!!");
+							Console.WriteLine("THE WINNER IS "+"bot"+battleID+"!!!");
 							break;
 						}
 					}
-					break;
 				}
-				
-				//while (bot_amount / 2 > 2)
-				//{
-				//	bot_amount--;
-				//}
-
 			}
-
-			/*while (TWO <= 2)
-			//{
-			if (dPower>60)
-			{
-				int attack = random.Next(50,100);
-				int bot_random = random.Next(1,5);
-				if (bot_random == 1)
-				{
-					int bot1 = 1;
-					Console.WriteLine("Lucky is "+bot1.ToString());
-					int bot_lucky = bot1;
-				}
-				if (bot_random == 2)
-				{
-					int bot2 = 2;
-					Console.WriteLine("Lucky is "+bot2.ToString());
-					int bot_lucky = bot2;
-				}
-				if (bot_random == 3)
-				{
-					int bot3 = 3;
-					Console.WriteLine("Lucky is "+bot3.ToString());
-					int bot_lucky = bot3;
-				}
-				if (bot_random == 4)
-				{
-					int bot4 = 4;
-					Console.WriteLine("Lucky is "+bot4.ToString());
-					int bot_lucky = bot4;
-				}
-				if (bot_random == 5)
-				{
-					int bot5 = 5;
-					Console.WriteLine("Lucky is "+bot5.ToString());
-					int bot_lucky = bot5;
-				}
-				//TWO++;
-			}
-
-			if (dPower<=60)
-			{
-				int attack_power = random.Next(20,90);
-				int bot_random = random.Next(1,5);
-				if (bot_random == 1)
-				{
-					int bot1 = 1;
-					Console.WriteLine("Not so lucky is "+bot1.ToString());
-					int bot_lucky = bot1;
-				}
-				if (bot_random == 2)
-				{
-					int bot2 = 2;
-					Console.WriteLine("Not so lucky is "+bot2.ToString());
-					int bot_lucky = bot2;
-				}
-				if (bot_random == 3)
-				{
-					int bot3 = 3;
-					Console.WriteLine("Not so lucky is "+bot3.ToString());
-					int bot_lucky = bot3;
-				}
-				if (bot_random == 4)
-				{
-					int bot4 = 4;
-					Console.WriteLine("Not so lucky is "+bot4.ToString());
-					int bot_lucky = bot4;
-				}
-				if (bot_random == 5)
-				{
-					int bot5 = 5;
-					Console.WriteLine("Not so lucky is "+bot5.ToString());
-					int bot_lucky = bot5;
-				}
-				//TWO++;
-			}
-			//}*/
 
 			Console.ReadLine();
 			
