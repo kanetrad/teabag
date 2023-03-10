d3321d2df3e662cab289b671c75b18b43b88d7a0
kanetrad <121792794+kanetrad@users.noreply.github.com>
2023-03-10 23:20:51 +0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -8,12 +8,14 @@ namespace main
 		{
 			Random random = new Random();
 
-			Console.WriteLine("Enter bot amount:");
+			Console.WriteLine("Enter round amount:");
 			int bot_amount = Int32.Parse(Console.ReadLine());
-			string bot;
-			string bot2;
+			string bot, bot2;
 			int winner;
-			Console.WriteLine(bot_amount);
+			int winner_score = 0;
+			int winner_score2 = 0;
+			int round = 0;
+			//Console.WriteLine(bot_amount);
 			for (int num=1; num<=bot_amount; num++)
             		{
 				Console.WriteLine(num);
@@ -30,19 +32,36 @@ namespace main
 			Console.WriteLine("Loop complete");
 			Console.WriteLine(" ");
 
-			for (int elim=bot_amount; elim>=0; elim--)
-            		{     
-				if (random.Next(1, 100) < 60)
-                		{
-                		winner = 1;
-				Console.WriteLine("In round "+round+" won bot");
-                	}
-				if (random.Next(1, 100) >= 60)
-				{
-					winner = 2;
-					Console.WriteLine("In round "+round+" won bot2");
+			for (int elim=bot_amount; elim>0; elim--)
+            		{
+            			for (int num=1; elim>0; num++)
+            			{
+            				while (round<bot_amount)
+					{
+						round++;
+
+						if (random.Next(1, 100) < 50)
+                				{
+                					winner = 1;
+                					winner_score++;
+							Console.WriteLine("In round "+round+" won FIRST bot");
+                				}
+						if (random.Next(1, 100) >= 50)
+						{
+							winner = 2;
+							winner_score2++;
+							Console.WriteLine("In round "+round+" won SECOND bot");
+						}
+					}
 				}
 			}
+
+			Console.WriteLine(" ");
+			Console.WriteLine("pretend that airplanes in the night sky..");
+			Console.WriteLine("oh (*___*)");
+			Console.WriteLine("i meant-");
+			Console.WriteLine("pretend that rounds' numbers are ordinal as usual");
+			Console.WriteLine("bc idk how to make them like that >.<");
 
 			Console.ReadLine();
 
